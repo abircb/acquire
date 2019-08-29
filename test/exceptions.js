17ce9ea5ffd96e5c195a093762f9f1419114b6f6
abir <abirchandrabhushan@gmail.com>
2019-08-29 13:59:29 +0530

    exception test complete

---
+++ test/exceptions.js
@@ -0,0 +1,46 @@
+const assert = require('assert')
+const should = require('should')
+const acquire = require('../')
+
+describe('no module information provided', function () {
+  it('throws an error', function () {
+    try {
+      acquire()
+    } catch (e) {
+      e.message.should.be.exactly('fatal: module name expected')
+    }
+  })
+})
+
+describe('unknown request throws', function () {
+  it('throws an error', function () {
+    try {
+      acquire('./test-node-modules/broken-module.js')
+      assert.fail('expected exception not thrown')
+    } catch (e) {
+      e.code.should.be.exactly('MODULE_NOT_FOUND')
+    }
+  })
+})
+
+describe('testing broken module', function () {
+  it('throws an error', function () {
+    try {
+      acquire('./some/wrong/name')
+    } catch (e) {
+      e.code.should.be.exactly('MODULE_NOT_FOUND')
+    }
+  })
+})
+
+describe('unknown request with path provided', function () {
+  it('throws an error', function () {
+    try {
+      acquire('./some/wrong/name', {
+        paths: 'some/place/that/does/not/exist'
+      })
+    } catch (e) {
+      e.code.should.be.exactly('MODULE_NOT_FOUND')
+    }
+  })
+})
