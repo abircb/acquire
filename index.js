6e4b963f58d694ca3af32dc205e3511d76388a3f
abir <abirchandrabhushan@gmail.com>
2019-08-29 11:08:37 +0530

    added supporting functions

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function acquire (moduleName, options) {
   const toArray = require('array-back')
   const prefix = options.prefix
   const paths = options.paths ? toArray(options.paths) : undefined
-  // const orignalModulePaths = module.paths
+  const orignalModulePaths = module.paths
   if (paths && paths.length) {
     module.paths = module.paths.concat(paths)
   }
@@ -29,7 +29,18 @@ function acquire (moduleName, options) {
         throw err
       }
     }
+  } else {
+    output = tryEachPath(moduleName, { paths })
+    if (output === null) {
+      output = loadAsLocalPath(moduleName, { paths })
+    }
+    if (output === null) {
+      output = loadAsRegularRequire(moduleName, { paths })
+    }
   }
+
+  module.paths = orignalModulePaths
+
   if (output === null) {
     const err = new Error('unable to find ' + moduleName)
     err.code = 'MODULE_NOT_FOUND'
@@ -37,6 +48,51 @@ function acquire (moduleName, options) {
   } else {
     return output
   }
+}
+
+function loadAsLocalPath (moduleName, options) {
+  const path = require('path')
+  let output
+  try {
+    output = require(path.resolve(moduleName), options)
+  } catch (err) {
+    if (err.code === 'MODULE_NOT_FOUND') {
+      output = null
+    } else {
+      throw err
+    }
+  }
+  return output
+}
+
+function loadAsRegularRequire (moduleName, options) {
+  let output
+  try {
+    /* workaround for node issue #28077 */
+    if (options && options.paths) {
+      output = require(require.resolve(moduleName, options))
+    } else {
+      output = require(require.resolve(moduleName))
+    }
+  } catch (err) {
+    if (err.code === 'MODULE_NOT_FOUND') {
+      output = null
+    } else {
+      throw err
+    }
+  }
+  return output
+}
+
+function tryEachPath (moduleName, options) {
+  const path = require('path')
+  let output = null
+  for (const p of options.paths || []) {
+    const fullPath = path.resolve(p, moduleName)
+    output = loadAsRegularRequire(fullPath)
+    if (output !== null) break
+  }
+  return output
 }
 
 module.exports = acquire
